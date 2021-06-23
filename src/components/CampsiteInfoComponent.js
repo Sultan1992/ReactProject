79dbb5716a19e4c80c382265251cfa749c6448bf
sultan <sultanfeto@gmail.com>
2021-06-23 18:56:31 -0500

    site info..is updated

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,27 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 
    
 
-   function RenderCampsite(campsite){
-        return (
-       <div className= "col-md-5 m-1">
-         <Card>
-                    <CardImg top src={campsite.image} alt={campsite.name} />
-                    <CardBody>
-                        <CardTitle>{campsite.name}</CardTitle>
-                        <CardText>{campsite.description}</CardText>
-                    </CardBody>
-                </Card>
+function RenderCampsite({campsite}){
+    return (
+        <div className="col-md-5 m-1">
+            <Card>
+                <CardImg top src={campsite.image} alt={campsite.name} />
+                <CardBody>
+                    <CardTitle>{campsite.name}</CardTitle>
+                    <CardText>{campsite.description}</CardText>
+                </CardBody>
+            </Card>
 
         </div>
        
-        )
+    );
 
     }
-  
-    //task 3
+
+   
  
 function RenderComments({comments}){
         if(comments){
@@ -57,9 +57,8 @@ function RenderComments({comments}){
             return (
                 <div className="container">
                 <div className="row">
-                     <RenderCampsite campsite={props.campsite}/>
-                     <RenderComments comments={props.campsite.comments}/>
-                     
+                        <RenderCampsite campsite={props.campsite} />
+                        <RenderComments comments={props.campsite.comments}/>
 
                 </div>
                 </div>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,8 +1,7 @@
 
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Directory from './DirectoryComponent';
-import CampsiteInfo from './CampsiteInfoComponent';
+import Contact from './ContactComponent';
 import { CAMPSITES } from '../shared/campsites';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
@@ -34,6 +33,7 @@ class Main extends Component {
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Route exact path='/contactus' component={Contact} />
                     <Redirect to='/home' />
                 </Switch>
                 <Footer />
