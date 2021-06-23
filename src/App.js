bd874bf9ed4a5ab97e7c5291824fbbee8d7b50da
sultan <sultanfeto@gmail.com>
2021-06-23 17:24:06 -0500

    React router added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
-import './App.css';
 import { CAMPSITES } from './shared/campsites';
 import Main from './components/MainComponent'
+import { BrowserRouter } from 'react-router-dom';
+import './App.css';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state={
-            campsites: CAMPSITES
-        }
-        }
+    
     render() {
         return (
+            <BrowserRouter>
             <div className="App">
                 <Main/>
-               </div>
+                </div>
+            </BrowserRouter>
         );
     }
 }
